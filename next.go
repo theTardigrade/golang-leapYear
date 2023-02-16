7b4c6ba4f6f593032ca16d15c5bf63ede4af83ad
James Smith <theTardigrade@users.noreply.github.com>
2023-02-16 09:29:12 +0000

    Fix mistake in documentation comments for Next and Prev functions

--- next.go
+++ next.go
@@ -2,6 +2,9 @@ package leapYear
 
 import "math"
 
+// Next returns the next leap year after a given year.
+// It also returns a boolean value that is set to false if no
+// such year can be found.
 func Next(givenYear int) (foundYear int, found bool) {
 	for foundYear = givenYear; foundYear < math.MaxInt; {
 		foundYear++
--- prev.go
+++ prev.go
@@ -2,7 +2,7 @@ package leapYear
 
 import "math"
 
-// Prev returns the previous leap year before a given leap year.
+// Prev returns the previous leap year before a given year.
 // It also returns a boolean value that is set to false if no
 // such year can be found.
 func Prev(givenYear int) (foundYear int, found bool) {
